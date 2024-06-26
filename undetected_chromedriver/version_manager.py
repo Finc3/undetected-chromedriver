0fc376c028c9e787d06382dc97ef64c2a0e7c324
Igor1Schambach <igorgitmail@gmail.com>
2024-06-26 12:58:25 +0200

    Updated versionamanger
    WIP versionamanager
    
    WIP
    
    Implemented VersionManager
    
    Implemented VersionManager
    
    WIP
    
    tets
    
    Testing

--- undetected_chromedriver/version_manager.py
+++ undetected_chromedriver/version_manager.py
@@ -1,17 +1,42 @@
 import os
+import pathlib
 import subprocess
+import time
 
-import chromedriver_autoinstaller
-from chromedriver_py import binary_path
 from common.util import logger
 
 
 class VersionManager:
     def __init__(self):
-        self.standard_path = binary_path
+        self.standard_path = os.path.join(str(pathlib.Path(__name__).parent.resolve()))
+        self.instances_path = f"{self.standard_path}/.ucdriver/instances"
+        self.purge_redundant_drivers()
+        self.version_file_path = self.version_file()
         self.chromedriver_version = self.get_chromedriver_version()
-        self.start()
-        self.get_executable_path()
+
+    def purge_redundant_drivers(self):
+        files = os.listdir(self.instances_path)
+        for _file in files:
+            os.remove(f"{self.instances_path}/{_file}")
+
+    def version_file(self):
+        path = os.path.join(str(pathlib.Path(__name__).parent.resolve()))
+        chromedriver_version = self.get_chromedriver_version()
+        if not chromedriver_version:
+            return
+        version_file_path = f"{path}/.ucdriver/version.txt"
+        version_exists = os.path.exists(version_file_path)
+
+        if not version_exists:
+            os.makedirs(os.path.dirname(version_file_path), exist_ok=True)
+            with open(version_file_path, "w") as f:
+                version = chromedriver_version
+                f.write(version)
+                return version
+        else:
+            with open(version_file_path, "r") as f:
+                version = f.read()
+                return version
 
     def get_installed_chrome_version(self):
         try:
@@ -24,44 +49,62 @@ class VersionManager:
 
     def get_chromedriver_version(self):
         try:
-            cmd = [self.standard_path, "--version"]
-            result = subprocess.run(cmd, capture_output=True, text=True)
-            return result.stdout.split()[1]
-        except IndexError:
+            files = os.listdir(self.instances_path)
+            files.sort()
+            if files:
+                first_instance = files[0]
+                cmd = [os.path.join(self.instances_path, first_instance), "--version"]
+                self.standard_path = os.path.join(self.instances_path, first_instance)
+                result = subprocess.run(cmd, capture_output=True, text=True)
+                return result.stdout.split()[1]
+            return False
+        except (IndexError, PermissionError, FileNotFoundError):
             logger.info("No ChromeDriver version found")
             return False
 
     def start(self):
-        if not self.chromedriver_version:
-            chromedriver_autoinstaller.install()
-            return self.get_executable_path()
         installed_chrome_version = self.get_installed_chrome_version()
-        if not installed_chrome_version:
+        if not installed_chrome_version and not self.chromedriver_version:
+            print("No Chrome installed or ChromeDriver version found")
+            self.install_chrome(version=None)
+        elif not installed_chrome_version and self.chromedriver_version:
+            print("No Chrome installed")
             self.install_chrome(self.chromedriver_version)
-        elif installed_chrome_version != self.chromedriver_version:
+        elif self.chromedriver_version and installed_chrome_version != self.chromedriver_version:
+            print("Chrome is not up to date")
             self.install_chrome(self.chromedriver_version)
         else:
             logger.info("Chrome is up to date")
-
-        return self.get_executable_path()
+        path = self.get_executable_path()
+        return path
 
     def install_chrome(self, version):
         if os.geteuid() != 0:
             logger.info("Insufficient rights")
         try:
-            chrome_download_url = f"https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_{version}-1_amd64.deb"  # https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb
-            chrome_installer_path = f"google-chrome-stable_{version}-1_amd64.deb"
-            # subprocess.run(["apt", "--fix-broken", "install", "-f"], check=True)  # sudo apt --fix-broken install
-            subprocess.run(["dpkg", "--configure", "--force-overwrite", "-a"], check=True)
-            subprocess.run(["dpkg", "-i", chrome_installer_path], check=True)
-            # subprocess.run(["apt-mark", "hold", "google-chrome-stable"], check=True)
+            if version:
+                chrome_download_url = f"https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_{version}-1_amd64.deb"  # https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb
+                chrome_installer_path = f"google-chrome-stable_{version}-1_amd64.deb"
+                subprocess.run(["apt", "install", "-y", f"./{chrome_installer_path}"], check=True)
+            else:
+                chrome_download_url = "https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_122.0.6261.128-1_amd64.deb"  #Just a test
+                subprocess.run(["wget", chrome_download_url], check=True)
+                result = subprocess.run(["apt", "install", "-y", "./google-chrome-stable_122.0.6261.128-1_amd64.deb"], check=True)
         except subprocess.CalledProcessError as e:
-            logger.info(f"Operation failed with error code: {e.returncode}...trying latest version")
-            chrome_download_url = "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"  # https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb
-            subprocess.run(["wget", chrome_download_url], check=True)  # sudo apt --fix-broken install
-            subprocess.run(["dpkg", "-i", "google-chrome-stable_current_amd64.deb"], check=True)
-            subprocess.run(["apt-get", "install", "-f"], check=True)
+            raise e
 
-    def get_executable_path(self):  # /snap/bin/chromium.chromedriver
-        return self.standard_path
-        # return ChromeDriverManager().install()  # TODO check where UC is installing the driver to
+        if self.confirm_installation(result):
+            return True
+
+    def confirm_installation(self, result):
+        while True:
+            try:
+                time.sleep(1)
+                if result.returncode == 0:
+                    return True
+            except subprocess.CalledProcessError:
+                logger.info("Chrome not installed yet")
+                continue
+
+    def get_executable_path(self):
+        return self.instances_path
