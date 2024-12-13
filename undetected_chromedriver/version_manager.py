6239e3c20567c573fc8129e9cd1694b0f88d1efe
Maksym Verchenko <verchenko1998@gmail.com>
2024-12-13 11:02:36 +0100

    added install for mac os

--- undetected_chromedriver/version_manager.py
+++ undetected_chromedriver/version_manager.py
@@ -84,7 +84,12 @@ class VersionManager:
                 return True
             
             if os.getenv("USE_MAC_QUEUE") == "true":
-                result = subprocess.run(["brew", "install", "--cask", "google-chrome"], check=True)
+                # result = subprocess.run(["brew", "install", "--cask", "google-chrome"], check=True)
+                chrome_download_url = "https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_131.0.6778.85-1_amd64.deb"
+                subprocess.run(["wget", chrome_download_url])
+                # subprocess.run(["dpkg", "--configure", "-a"])
+                result = subprocess.run(["apt-get", "install","-f", "-y", "./google-chrome-stable_131.0.6778.85-1_amd64.deb"])
+                os.remove("google-chrome-stable_131.0.6778.85-1_amd64.deb")
             else:
                 if version:
                     chrome_download_url = f"https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_{version}_amd64.deb"
